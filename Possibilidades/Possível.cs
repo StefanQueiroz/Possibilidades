b04a5388c104d7a87a7616a9a3ea600ead6fe0c9
Stefan Cardoso Queiroz <stefanqueiroz@yahoo.com.br>
2016-11-23 14:15:46 -0200

    Atualização Geral
    Subindo o Primeiro Código

---
+++ Possibilidades/Possível.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace Possibilidades
+{
+    public class Possível
+    {
+        private char[] dicionario;
+        private ulong qtdCaracteres;
+        private ulong possibilidades;
+        private ulong index = 0;
+        private StringBuilder sb = new StringBuilder();
+
+        public bool hasNext = true;
+
+        public Possível(string dicionario, ulong qtdCaracteres)
+        {
+            this.dicionario = dicionario.ToCharArray();
+            this.qtdCaracteres = qtdCaracteres;
+            getPossibilidades();
+        }
+
+        private void getPossibilidades()
+        {
+            ulong temp = (ulong)dicionario.Length;
+            possibilidades = temp;
+            for (ulong i = 1; i < qtdCaracteres; i++)
+            {
+                possibilidades = possibilidades * temp;
+            }
+        }
+
+        public string getNext()
+        {
+            sb.Clear();
+            for (ulong ii = 0; ii < qtdCaracteres; ii++)
+            {
+                sb.Append(dicionario[(int)((index / Math.Pow(dicionario.Length, ii)) % dicionario.Length)]);
+            }
+            index++;
+            if (!(index < possibilidades))
+            {
+                hasNext = false;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Teste/Program.cs
@@ -0,0 +1,20 @@
+﻿using Possibilidades;
+using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Possível p = new Possível("asdfgh",3);
+
+            while (p.hasNext)
+            {
+                Console.WriteLine(p.getNext());
+            }
+            
+            Console.ReadLine();       
+        }
+    }
+}
